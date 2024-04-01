6211d9ea95f230e963d040c38de089463edb514d
isaiahyesufu <115463420+isaiahyesufu@users.noreply.github.com>
2024-04-01 16:55:33 +0100

    Update currencyConverter.java

--- currencyConverter.java
+++ currencyConverter.java
@@ -31,8 +31,8 @@ public class currencyConverter {
     }
 
     public static void Naira_to_other(double amt) {
-        double nairaToDollarRate = 0.0023; 
-        double nairaToEuroRate = 0.0022; 
+        double nairaToDollarRate = 0.0023;
+        double nairaToEuroRate = 0.0022;
         System.out.println("1 Naira = " + nairaToDollarRate + " Dollar");
         System.out.println();
 
@@ -47,12 +47,14 @@ public class currencyConverter {
     }
 
     public static void Dollar_to_other(double amt) {
-        System.out.println("1 Dollar = " + 445.54 + " Naira"); 
+        System.out.println("1 Dollar = " + 445.54 + " Naira");
         System.out.println();
         System.out.println(amt + " Dollar = " + (amt * 445.54) + " Naira");
         System.out.println();
 
-        
+
+
+
     }
 
     public static void Euro_to_other(double amt) {
@@ -61,6 +63,6 @@ public class currencyConverter {
         System.out.println(amt + " Euro = " + (amt * 460.27) + " Naira");
         System.out.println();
 
-        
+
     }
 }
